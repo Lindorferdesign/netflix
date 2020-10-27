c59bc7168c18d940759a1c935c35034820523d45
Daniel Lindorfer <daniellindorfer@Daniels-MacBook-Pro.local>
2020-10-27 19:07:46 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import GlobalStyles from './global-styles';
+
+ReactDOM.render(<>
+    <GlobalStyles />
+    <App />
+    </>,
+    document.getElementById('root'));
